065045bc0a89932f0e8d6686d4c3dfbbae9c42c1
Kirill <kdehterenok@gmail.com>
2026-03-07 16:15:49 +0300

    Save main

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите ваше число: ");
+        int x = sc1.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("Введенное число " + x + " чётное\n");
+        } else {
+            System.out.println("Введенное число " + x + " нечётное\n");
+        }
+
+        System.out.println("Задание 2");
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите вашу температуру: ");
+        int t = sc2.nextInt();
+        if (t > -5) {
+            System.out.println("Warm\n");
+        } else if (t <= -5 & t > -20) {
+            System.out.println("Normal\n");
+        } else {
+            System.out.println("Cold\n");
+        }
+
+        System.out.println("Задание 3");
+
+        for(int i = 10; i <= 20; ++i) {
+            System.out.println(i * i);
+        }
+
+        System.out.println("\n");
+        System.out.println("Задание 4");
+        int j = 7;
+
+        for(int n = 1; j * n <= 98; ++n) {
+            System.out.println(j * n);
+        }
+
+        System.out.println("\n");
+        Scanner sc3 = new Scanner(System.in);
+        System.out.println("Введите целое положительное число: ");
+        double k = sc3.nextDouble();
+        int l = 0;
+        if (!(k <= (double)0.0F) && k % (double)1.0F == (double)0.0F) {
+            for(int z = 1; (double)z <= k; ++z) {
+                l += z;
+                if ((double)z == k) {
+                    System.out.println(z + " = " + l);
+                    break;
+                }
+
+                System.out.print(z + " + ");
+            }
+        } else {
+            System.out.println("Данные введены некорректно! Введите ЦЕЛОЕ и ПОЛОЖИТЕЛЬНОЕ число");
+        }
+
+    }
+}
